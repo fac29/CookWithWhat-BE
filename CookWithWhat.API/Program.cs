f324dc17174260b52fe1f9b90e22529615af4d0c
AlexVOiceover <alex@alexvoiceover.com>
2024-07-16 20:33:30 +0100

    Revert "Merge branch 'main' into AlexTemp"
    This reverts commit 9b043af4ce7b9b3494752f3562bdad6eaeb62c21, reversing
    changes made to 04d570776a4207b6dd79cfd69ee185860a9c2c27.

--- CookWithWhat.API/Program.cs
+++ CookWithWhat.API/Program.cs
@@ -12,11 +12,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
-
 builder.Services.AddDbContext<CookWithWhatDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
-
 
 var app = builder.Build();
 
