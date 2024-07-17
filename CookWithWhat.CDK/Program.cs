3c4f95b9b5ca78eaa8eede0fec2ad152702b9934
AlexVOiceover <alex@alexvoiceover.com>
2024-07-17 10:15:32 +0100

    Created CDK Project

---
+++ CookWithWhat.CDK/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.CDK;
+
+namespace CookWithWhat.CDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App();
+            new CookWithWhatStack(app, "CookWithWhatStack");
+            app.Synth();
+        }
+    }
+}
